8c693029490a663f81e03d51ff6ec386a27df678
Cristian <cristianorellana@itstechtalentfactory.it>
2025-12-21 18:46:04 +0100

    Commit: Update the base structure to maven project

---
+++ src/main/java/it/homegym/controller/TestServlet.java
@@ -0,0 +1,16 @@
+package it.homegym.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.getWriter().println("HomeGym OK!");
+    }
+}
